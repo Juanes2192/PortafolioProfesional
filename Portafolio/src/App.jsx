dc33a62c8206268cbb353683a91e5727b33acc46
Juanes2192 <juanestebanperdomoleiva@gmail.com>
2024-05-29 03:52:59 -0500

    Se agrega info mia y ajuste de fondo

--- Portafolio/src/App.jsx
+++ Portafolio/src/App.jsx
@@ -1,14 +1,85 @@
 import React from 'react';
-import './App.css'
-import { NavBar } from './components';
+import NavBar from './components/NavBar/NavBar';
+import "./App.css";
+import foto from './img/FotoCV.jpg';
+import logohtml from './img/logohtml.svg';
+import logocss from './img/CSS-Logo.svg';
+import logoreact from './img/Reactlogo.svg';
+import logobootstrap from './img/Bootstrap_logo.svg';
+import logogithub from './img/Github-logo.svg';
+import fondo from './img/fondoportafolio.jpg'
 
-export function App() {
+function App() {
   return (
     <div>
-      <NavBar nombre='Esteban' apellido='Perdomo'/>
-      <NavBar nombre='Felipe' apellido='Rodirguez'/>
+      <header className='Header'>
+        <NavBar />
+      </header>
+      <div className='contenedor-principal'>
+        <div className='secciones'>
+
+        <section id="inicio" style={{backgroundImage: `url(${fondo})`}}>
+            <div className="circle">
+              <img src={foto} alt="FotoCV" className="image" />
+            </div>
+            <div className='secciones seccion-inicio'>
+              <div className="presentacion">
+                <h3 className='presentacion saludo'>Hola!</h3>
+                <h1 className='presentacion nombre'>Soy Esteban Perdomo</h1>
+                <h3 className='presentacion profesion'>Desarollador Frontend</h3>
+              </div>
+            </div>
+          </section>
+
+          <section id="acerca-de-mi">
+            <div className='secciones seccion-acerca-de-mi'>
+              <h2>Acerca de mi</h2>
+              <h5>Soy Esteban Perdomo / Desarrollador Frontend / Tecnologa en desarrollo de software</h5>
+              <p>¡Hola! Soy Esteban Perdomo, desarrollador Frontend con formación en desarrollo de software. 
+                Me apasiona crear experiencias web excepcionales y cautivadoras. Mi enfoque se centra en combinar 
+                habilidades técnicas con diseño intuitivo para ofrecer interfaces atractivas y funcionales. 
+                ¡Estoy emocionada de colaborar contigo y llevar tus ideas al mundo digital!</p>
+            </div>
+          </section>
+
+          <section id="mis-habilidades">
+            <div className='secciones seccion-mis-habilidades'>
+              <h2>Mis Habilidades</h2>
+              <div className="logos-container">
+                <div className="logo">
+                  <img src={logohtml} alt="HTML" />
+                </div>
+                <div className="logo">
+                  <img src={logocss} alt="CSS" />
+                </div>
+                <div className="logo">
+                  <img src={logoreact} alt="React" />
+                </div>
+                <div className="logo">
+                  <img src={logobootstrap} alt="Bootstrap" />
+                </div>
+                <div className="logo">
+                  <img src={logogithub} alt="GitHub" />
+                </div>
+              </div>
+            </div>
+          </section>
+
+          <section id="proyectos">
+            <div className='secciones seccion-proyectos'>
+              <h2>Proyectos</h2>
+            </div>
+          </section>
+
+          <section id="contacto">
+            <div className='secciones seccion-contacto'>
+              <h2>Contacto</h2>
+            </div>
+          </section>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default App;
--- Portafolio/src/components/NavBar/NavBar.jsx
+++ Portafolio/src/components/NavBar/NavBar.jsx
@@ -1,16 +1,39 @@
-import "./NavBar.css"
+import React from 'react';
+import { Navbar, Nav } from 'react-bootstrap'; // Importa componentes de la barra de navegación de React Bootstrap
+import Container from 'react-bootstrap/Container'; // Importa el componente Container de React Bootstrap
+import "./NavBar.css"; // Importa los estilos CSS para la barra de navegación
 
-export function NavBar({nombre,apellido}) {
+export function NavBar() {
 
-    console.log(nombre);
-    console.log(apellido);
-    
+  // Función para ajustar el desplazamiento después de hacer clic en un enlace
+  const handleNavLinkClick = (event, targetId) => {
+    event.preventDefault(); // Evita el comportamiento predeterminado del enlace
+    const targetElement = document.getElementById(targetId); // Obtiene el elemento de destino utilizando su ID
+    if (targetElement) { // Verifica si el elemento de destino existe
+      window.scrollTo({
+        top: targetElement.offsetTop - 100, // Ajusta el desplazamiento para tener un poco de espacio sobre el título
+        behavior: "smooth" // Desplazamiento suave
+      });
+    }
+  };
+
   return (
-    <div>
-        <h1>Nombre: {nombre}</h1>
-        <h2>Apellido: {apellido}</h2>
-        <h2>sdsd</h2>
-    </div>
+    <Navbar expand="lg" className="bg-body-tertiary"> 
+      <Container> 
+        <Navbar.Brand href="#home">{"< EP />"}</Navbar.Brand> 
+        <Navbar.Toggle aria-controls="basic-navbar-nav" /> 
+        <Navbar.Collapse id="basic-navbar-nav"> 
+          <Nav className="me-auto"> 
+            {/* Enlaces de navegación con identificadores y manejadores de eventos para el clic */}
+            <Nav.Link onClick={(e) => handleNavLinkClick(e, 'inicio')} href="#inicio">Inicio</Nav.Link>
+            <Nav.Link onClick={(e) => handleNavLinkClick(e, 'acerca-de-mi')} href="#acerca-de-mi">Acerca de mi</Nav.Link>
+            <Nav.Link onClick={(e) => handleNavLinkClick(e, 'mis-habilidades')} href="#mis-habilidades">Mis Habilidades</Nav.Link>
+            <Nav.Link onClick={(e) => handleNavLinkClick(e, 'proyectos')} href="#proyectos">Proyectos</Nav.Link>
+            <Nav.Link onClick={(e) => handleNavLinkClick(e, 'contacto')} href="#contacto">Contactos</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
 
